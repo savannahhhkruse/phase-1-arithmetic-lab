d142c2dec90967f2d37f50b53a92a9568a6d4584
Savannah <skruse101@gmail.com>
2023-03-27 18:14:38 -0700

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 
 // Write your code here
+
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+// console.log(multiply);
+
+let random = Math.floor(Math.random() * 10) +1;
+
+// console.log(random);
+
+let num3 = 12;
+let num4 = 4;
+
+let mod = num4 % num3;
+
+// console.log(mod);
+
+let num5 = 5;
+let num6 = 20;
+
+let max = Math.max(num5, num6);
+
+// console.log(max);
+
+// let min = Math.min(num5, num6);
+// console.log(min);
